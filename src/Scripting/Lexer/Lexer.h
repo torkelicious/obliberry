7405420d9688a65e7278c65ff08fd0fe5ec9f834
torkelicious <torkel.ivinger@elev.ga.lbs.se>
2026-06-13 19:41:16 +0200

    Update Lexer.h

--- src/Scripting/Lexer/Lexer.h
+++ src/Scripting/Lexer/Lexer.h
@@ -69,22 +69,32 @@ namespace Scripting {
     class Lexer {
     public:
         Lexer(std::string_view source);
+
         std::vector<Token> tokenize();
+
     private:
         std::string_view source;
-        size_t start = 0;
+        //size_t start = 0; // unused for now but will pop up later
         size_t current = 0;
         int line = 1;
         int column = 1;
 
         char peek() const;
+
         char peek_next() const;
+
         char advance();
+
         bool is_at_end() const;
+
         void skip_whitespace();
+
         Token read_string();
+
         Token read_num();
+
         Token read_identifier_or_keyword();
+
         Token read_operator_or_symbol();
     };
 }
