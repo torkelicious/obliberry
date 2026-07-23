7622d448348aba70092d3ac5fc393f82268237f5
torkelicious <torkelivinger@proton.me>
2026-07-23 13:33:11 +0200

    ui docking settings

--- src/Applications/Editor/EditorLayer.cpp
+++ src/Applications/Editor/EditorLayer.cpp
@@ -390,8 +390,8 @@ void Editor::EditorLayer::DrawDockSpace() {
     const ImGuiID dock_id_bottom = ImGui::DockBuilderSplitNode(dock_id_center, ImGuiDir_Down, 0.3f, nullptr, &dock_id_center);
 
     ImGui::DockBuilderDockWindow("Registry", dock_id_left);
+    ImGui::DockBuilderDockWindow("UI Hierarchy", dock_id_left);
     ImGui::DockBuilderDockWindow("Inspector", dock_id_right);
-    ImGui::DockBuilderDockWindow("UI Hierarchy", dock_id_right);
     ImGui::DockBuilderDockWindow("Console", dock_id_bottom);
     ImGui::DockBuilderDockWindow("Project Browser", dock_id_bottom);
     ImGui::DockBuilderDockWindow("Scene View", dock_id_center);
--- src/Scripting/EngineLib/Modules/EngineLibUI.cpp
+++ src/Scripting/EngineLib/Modules/EngineLibUI.cpp
@@ -1,4 +1,3 @@
-#include <imgui.h>
 #include <mutex>
 #include "Scripting/EngineLib/EngineLib.h"
 #include <ObSL/Interpreter.h>
@@ -8,16 +7,6 @@ namespace {
 }
 
 void Scripting::EngineLib::register_gui_modules(ObSL::Interpreter &interpreter) {
-    // temp imgui binding for testing type stuff :)
-    interpreter.get_global_environment()->define("ImGui_Button", interpreter.gc.allocate<ObSL::NativeFunction>(
-                                                     1,
-                                                     [](ObSL::Interpreter *,
-                                                        const std::vector<ObSL::Value> &args) -> ObSL::Value {
-                                                         if (args.empty() || !std::holds_alternative<std::string>(
-                                                                 args[0]))
-                                                             return false;
-                                                         std::lock_guard lock(s_GuiMutex);
-                                                         return ImGui::Button(std::get<std::string>(args[0]).c_str());
-                                                     }, "ImGui_Button"));
+
 }
 
