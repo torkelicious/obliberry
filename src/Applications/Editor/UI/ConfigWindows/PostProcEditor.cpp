a60d44dcea8ab3d0ef519f8e68ceb24b3ace57d2
torkelicious <torkelivinger@proton.me>
2026-09-01 08:26:18 +0200

    wip uniforms ui & wip direct apply

--- src/Applications/Editor/UI/ConfigWindows/PostProcEditor.cpp
+++ src/Applications/Editor/UI/ConfigWindows/PostProcEditor.cpp
@@ -5,43 +5,79 @@
 #include "Rendering/Renderer.h"
 #include <imgui.h>
 
-// todo: uniforms and shi
+// todo: finish ts bs
 
 namespace Editor::UI {
-    namespace {
-        void DrawFXEntry(std::vector<Rendering::PostProcessing::PostEffect> &fxVec, const std::size_t index) {
-            auto &fx = fxVec[index];
-            ImGui::PushID(fx.shaderKey.c_str());
 
-            ImGui::SetNextItemAllowOverlap();
+    void PostProcEditor::DrawFXEntry(std::vector<Rendering::PostProcessing::PostEffect> &fxVec, const std::size_t index) {
+        auto &fx = fxVec[index];
+        ImGui::PushID(fx.shaderKey.c_str());
 
-            // render selectable
-            ImGui::Selectable(fx.shaderKey.c_str(), false, 0, ImVec2(200.0f, 0.0f));
+        ImGui::SetNextItemAllowOverlap();
 
-            // tetup drag source
-            if (ImGui::BeginDragDropSource(ImGuiDragDropFlags_None)) {
-                ImGui::SetDragDropPayload("POST_PROC_FX", &index, sizeof(std::size_t));
-                ImGui::Text("Moving %s", fx.shaderKey.c_str());
-                ImGui::EndDragDropSource();
-            }
+        // render selectable
+        ImGui::Selectable(fx.shaderKey.c_str(), false, 0, ImVec2(200.0f, 0.0f));
 
-            // setup drag  target
-            if (ImGui::BeginDragDropTarget()) {
-                if (const ImGuiPayload *payload = ImGui::AcceptDragDropPayload("POST_PROC_FX")) {
-                    IM_ASSERT(payload->DataSize == sizeof(std::size_t));
-                    const std::size_t payloadIndex = *static_cast<const std::size_t *>(payload->Data);
-
-                    Core::Utils::vector::moveItem(fxVec, payloadIndex, index);
-                }
-                ImGui::EndDragDropTarget();
-            }
-
-            ImGui::SameLine();
-            ImGui::Checkbox("Enabled", &fx.enabled);
-
-            ImGui::PopID();
+        // tetup drag source
+        if (ImGui::BeginDragDropSource(ImGuiDragDropFlags_None)) {
+            ImGui::SetDragDropPayload("POST_PROC_FX", &index, sizeof(std::size_t));
+            ImGui::EndDragDropSource();
         }
-    } // namespace
+
+        // setup drag  target
+        if (ImGui::BeginDragDropTarget()) {
+            if (const ImGuiPayload *payload = ImGui::AcceptDragDropPayload("POST_PROC_FX")) {
+                IM_ASSERT(payload->DataSize == sizeof(std::size_t));
+                if (const std::size_t payloadIndex = *static_cast<const std::size_t *>(payload->Data); payloadIndex != index) {
+                    Core::Utils::vector::moveItem(fxVec, payloadIndex, index);
+                    m_Dirty = true;
+                }
+            }
+
+            ImGui::EndDragDropTarget();
+        }
+
+
+        ImGui::SameLine();
+        ImGui::Checkbox("Enabled", &fx.enabled);
+
+        if (!fx.uniforms.empty()) {
+            if (ImGui::CollapsingHeader("Uniforms")) {
+                UniformMapToImGui(fx.uniforms);
+            }
+        }
+
+        ImGui::PopID();
+    }
+
+    void PostProcEditor::UniformMapToImGui(std::unordered_map<std::string, Rendering::PostProcessing::UniformValue> &uniforms) {
+        if (uniforms.empty())
+            return;
+
+        for (auto &[key, value] : uniforms) {
+            std::visit(
+                    [&]<typename T0>(T0 &val) {
+                        using T = std::decay_t<T0>;
+                        if constexpr (std::is_same_v<T, float>) {
+                            if (ImGui::DragFloat(key.c_str(), &val))
+                                m_Dirty = true;
+                        } else if constexpr (std::is_same_v<T, int>) {
+                            if (ImGui::DragInt(key.c_str(), &val))
+                                m_Dirty = true;
+                        } else if constexpr (std::is_same_v<T, glm::vec2>) {
+                            if (ImGui::DragFloat2(key.c_str(), &val.x))
+                                m_Dirty = true;
+                        } else if constexpr (std::is_same_v<T, glm::vec3>) {
+                            if (ImGui::DragFloat3(key.c_str(), &val.x))
+                                m_Dirty = true;
+                        } else if constexpr (std::is_same_v<T, glm::vec4>) {
+                            if (ImGui::DragFloat4(key.c_str(), &val.x))
+                                m_Dirty = true;
+                        }
+                    },
+                    value);
+        }
+    }
 
     void PostProcEditor::OnImGuiRender(bool &isOpen) {
         if (!isOpen || !m_Context->renderer)
@@ -60,12 +96,12 @@ namespace Editor::UI {
         if (ImGui::Button("Apply")) {
             SaveConfig();
         }
-
+        ImGui::SameLine();
         if (ImGui::Button("Save & Close")) {
             SaveConfig();
             isOpen = false;
         }
-
+        ImGui::SameLine();
         if (ImGui::Button("Close")) {
             isOpen = false;
         }
@@ -73,11 +109,27 @@ namespace Editor::UI {
         ImGui::End();
     }
 
+    // apply inplace without going through undomgr
+    void PostProcEditor::Apply() {
+        m_Context->renderer->GetPostProcessor().Effects() = m_StagingVec;
+        m_Dirty = true;
+    }
+
+    // restore without going through undomgr
+    void PostProcEditor::Restore() {
+        m_Context->renderer->GetPostProcessor().Effects() = m_fxVecBackup;
+        m_Dirty = false;
+    }
+
     void PostProcEditor::Reload() {
         m_fxVecBackup = m_Context->renderer->GetPostProcessor().Effects();
         m_StagingVec = m_fxVecBackup;
+        m_Dirty = false;
     }
 
-    void PostProcEditor::SaveConfig() { m_Undomgr->Execute(std::make_unique<Commands::PostProcUpdateCommand>(m_fxVecBackup, m_StagingVec), *m_Context); }
+    void PostProcEditor::SaveConfig() {
+        m_Undomgr->Execute(std::make_unique<Commands::PostProcUpdateCommand>(m_fxVecBackup, m_StagingVec), *m_Context);
+        m_Dirty = false;
+    }
 
 } // namespace Editor::UI
--- src/Applications/Editor/UI/ConfigWindows/PostProcEditor.h
+++ src/Applications/Editor/UI/ConfigWindows/PostProcEditor.h
@@ -6,13 +6,20 @@ namespace Editor ::UI {
 
     class PostProcEditor : public ConfigEditor {
     public:
+        void DrawFXEntry(std::vector<Rendering::PostProcessing::PostEffect> &fxVec, const std::size_t index);
+        void UniformMapToImGui(std::unordered_map<std::string, Rendering::PostProcessing::UniformValue> &uniforms);
         void OnImGuiRender(bool &isOpen) override;
         void Reload() override;
         void SaveConfig() override;
 
+        // inplace w.out undomgr
+        void Apply();
+        void Restore();
+
     private:
         std::vector<Rendering::PostProcessing::PostEffect> m_fxVecBackup; // m_OldData
         std::vector<Rendering::PostProcessing::PostEffect> m_StagingVec;  // m_LocalData
+        bool m_Dirty = false;
     };
 
 } // namespace Editor::UI
