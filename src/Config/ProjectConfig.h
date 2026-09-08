ceb09505d63f3732c45df85e65daea2ec826084f
torkelicious <torkelivinger@proton.me>
2026-09-08 19:27:24 +0200

    Update ProjectConfig.h
    windows dasfgvkljhdgafjhkdgfklhjdafgljkhadfgljkhadgfjklhadfgljkhadfglhjdgaf1

--- src/Config/ProjectConfig.h
+++ src/Config/ProjectConfig.h
@@ -11,6 +11,10 @@ namespace Config {
         // default to the central relative project tag identifier
         static ProjectConfig Deserialize(const std::string &filepath = "project.json");
 
+        static ProjectConfig Deserialize(const std::filesystem::path &filepath = "project.json") { return Deserialize(filepath.string()); }
+
         static bool Serialize(const ProjectConfig &conf, const std::string &filepath = "project.json");
+
+        static bool Serialize(const ProjectConfig &conf, const std::filesystem::path &filepath = "project.json") { return Serialize(conf, filepath.string()); }
     };
 } // namespace Config
