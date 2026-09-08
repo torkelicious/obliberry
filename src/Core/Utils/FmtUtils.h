dbb73ce4c5f071e63462a6d4288326430be878e7
torkelicious <torkelivinger@proton.me>
2026-09-08 19:10:23 +0200

    macos sdfaglöjkasdgfhjködfjklagh

--- src/Core/Utils/FmtUtils.h
+++ src/Core/Utils/FmtUtils.h
@@ -1,3 +1,5 @@
+#pragma once
+
 #include <chrono>
 #include <ctime>
 #include <filesystem>
@@ -6,18 +8,17 @@
 namespace Core::Utils::Fmt {
 
     inline std::string formatFileTimestamp(std::filesystem::file_time_type time) {
-        const auto systemTime = std::chrono::clock_cast<std::chrono::system_clock>(time);
+        const auto fileNow = std::filesystem::file_time_type::clock::now();
+        const auto sysNow = std::chrono::system_clock::now();
+        const auto sysTime = sysNow + std::chrono::duration_cast<std::chrono::system_clock::duration>(time - fileNow);
 
-        const std::time_t timeT = std::chrono::system_clock::to_time_t(systemTime);
-
+        const std::time_t timeT = std::chrono::system_clock::to_time_t(sysTime);
         std::tm tm{};
-
-#ifdef _WIN32
+#if defined(_WIN32)
         localtime_s(&tm, &timeT);
 #else
         localtime_r(&timeT, &tm);
 #endif
-
         char buffer[64]{};
         std::strftime(buffer, sizeof(buffer), "%Y-%m-%d %H:%M:%S", &tm);
 
