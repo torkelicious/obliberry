c674216de5686d9018eaf5bb86734ff3ed040a88
torkelicious <torkelivinger@proton.me>
2026-09-08 17:25:48 +0200

    formatting

--- src/UI/Elements/UIImage.h
+++ src/UI/Elements/UIImage.h
@@ -4,19 +4,25 @@
 #include <memory>
 
 namespace UI {
-    class UIImage : public UIElement {
-    public:
-        void SetImage(const std::shared_ptr<Rendering::Texture> &tex) { m_Image = tex; }
-        void SetColor(const glm::vec4 color) { m_Color = color; }
-        [[nodiscard]] const glm::vec4 &GetColor() const { return m_Color; }
-        [[nodiscard]] const std::shared_ptr<Rendering::Texture> &GetImage() const { return m_Image; }
-        [[nodiscard]] std::shared_ptr<Rendering::Texture> &GetImage() { return m_Image; }
+class UIImage : public UIElement {
+public:
+  void SetImage(const std::shared_ptr<Rendering::Texture> &tex) {
+    m_Image = tex;
+  }
+  void SetColor(const glm::vec4 color) { m_Color = color; }
+  [[nodiscard]] const glm::vec4 &GetColor() const { return m_Color; }
+  [[nodiscard]] const std::shared_ptr<Rendering::Texture> &GetImage() const {
+    return m_Image;
+  }
+  [[nodiscard]] std::shared_ptr<Rendering::Texture> &GetImage() {
+    return m_Image;
+  }
 
-        void Update() override;
-        void Draw(UIRenderer *renderer, glm::vec2 finalPos) override;
+  void Update() override;
+  void Draw(UIRenderer *renderer, glm::vec2 finalPos) override;
 
-    private:
-        std::shared_ptr<Rendering::Texture> m_Image;
-        glm::vec4 m_Color = {1.0f, 1.0f, 1.0f, 1.0f};
-    };
-}
+private:
+  std::shared_ptr<Rendering::Texture> m_Image;
+  glm::vec4 m_Color = {1.0f, 1.0f, 1.0f, 1.0f};
+};
+} // namespace UI
