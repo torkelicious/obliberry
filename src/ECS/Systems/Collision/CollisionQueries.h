35c47c0f5b238f0c68ed9670df32c4d8b92d60b5
torkelicious <torkelivinger@proton.me>
2026-09-13 09:54:44 +0200

    Create CollisionQueries.h

---
+++ src/ECS/Systems/Collision/CollisionQueries.h
@@ -0,0 +1,65 @@
+#pragma once
+
+#include "ColliderGeometry.h"
+#include "ECS/Components/ColliderComponent.h"
+#include "ECS/Components/TransformComponent.h"
+#include "ECS/Entity.h"
+#include "ECS/Types.h"
+#include "GJK.h"
+#include "ECS/Registry.h"
+#include "glm/ext/vector_double3.hpp"
+
+namespace ECS::Collision {
+
+    inline bool CanOccupy(Registry &registry, EntityID entity, const glm::vec3 &targetWorldPos, const BillboardBasis &basis) {
+        const auto *collider = registry.GetComponent<Components::ColliderComponent>(entity);
+        const auto *transform = registry.GetComponent<Components::TransformComponent>(entity);
+
+        if (!transform) {
+            return false;
+        }
+        if (!collider || collider->isTrigger) {
+            return true;
+        }
+
+        if (!Components::IsValidCollider(*collider)) {
+            return false;
+        }
+
+        WorldCollider cand = BuildWorldCollider(entity, *collider, *transform, basis);
+
+        // world space
+        const glm::dvec3 displace = glm::dvec3(targetWorldPos) - glm::dvec3(transform->worldTransform.GetMatrix()[3]);
+
+        cand.localToWorld[3] = glm::dvec4(displace, 0.0);
+
+        const ColliderAABB candBounds = ComputeWorldAABB(cand);
+
+        bool blocked = false;
+
+        registry.ForEach<Components::ColliderComponent, Components::TransformComponent>([&](const Entity otherEnt, const Components::ColliderComponent *othercCol, const Components::TransformComponent *otherTrans) {
+            if (blocked) {
+                return;
+            }
+
+            const EntityID otherId = static_cast<EntityID>(otherEnt);
+            if (otherId == entity || othercCol->isTrigger) {
+                return;
+            }
+
+            const WorldCollider other = BuildWorldCollider(otherId, *othercCol, *otherTrans, basis);
+
+            if (!OverlapsAABB(candBounds, ComputeWorldAABB(other))) {
+                return;
+            }
+
+            const GJKResult result = IntersectsGJK(cand, other);
+
+            blocked = result != GJKResult::Separated;
+        });
+
+        return !blocked;
+    }
+
+
+} // namespace ECS::Collision
