4d24cc7eefc499c0f3a2ca0d33211bba022d3acf
torkelicious <torkelivinger@proton.me>
2026-09-16 23:04:08 +0200

    format

--- src/ECS/Components/SpriteAnimatorComponent.h
+++ src/ECS/Components/SpriteAnimatorComponent.h
@@ -5,18 +5,18 @@
 
 namespace ECS::Components {
 
-struct SpriteAnimatorComponent {
-    std::shared_ptr<const Animation::SpriteAnimationSet> animations;
+    struct SpriteAnimatorComponent {
+        std::shared_ptr<const Animation::SpriteAnimationSet> animations;
 
-    // saved config
-    std::string initialClip;
-    bool autoplay = true;
+        // saved config
+        std::string initialClip;
+        bool autoplay = true;
 
-    std::string clip;
-    size_t frameIndex = 0;
-    double elapsed = 0.0;
-    bool playing = false;
-};;
+        std::string clip;
+        size_t frameIndex = 0;
+        double elapsed = 0.0;
+        bool playing = false;
+    };
 
 
 } // namespace ECS::Components
--- src/ECS/Systems/Animation/Types.h
+++ src/ECS/Systems/Animation/Types.h
@@ -5,6 +5,7 @@
 #include <memory>
 #include <unordered_map>
 #include <vector>
+#include <string>
 
 namespace Animation {
     struct SpriteFrame {
